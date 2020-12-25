5b43b119e3d9fcd011849ac8b1d2548e5fc8aaf1
Daiwa <open159632@gmail.com>
2020-12-26 02:52:59 +0800

    新增資料庫ADD以及delete功能

---
+++ backend/db_function.py
@@ -0,0 +1,29 @@
+from db_model import Club, Student, db, Indentity
+
+def add(student_id, club_id, student_name, club_name, departments, job):
+
+    if Student.query.filter_by(std_id = student_id).first() == None:
+        student = Student(std_id = student_id, department = departments, name = student_name)
+    else:
+        student = Student.query.filter_by(name = student_name).first()
+
+    if Club.query.filter_by(name = club_name).first() == None:
+        club = Club(club_id = club_id, name = club_name)
+    else:
+        club = Club.query.filter_by(name = club_name).first()
+
+    indentity = Indentity(job = job)
+
+    club.indentitys.append(indentity)
+    student.indentitys.append(indentity)
+    db.session.add(club)
+    db.session.add(student)
+    db.session.add(indentity)
+    db.session.commit()
+
+def delete(student_id, club_id, job):
+
+    delete_data = Indentity.query.filter_by(std_id = student_id, club_id = club_id, job = job).first()
+    db.session.delete(delete_data)
+    db.session.commit()
+
---
+++ backend/db_model.py
@@ -0,0 +1,32 @@
+from flask_sqlalchemy import SQLAlchemy
+from flask import Flask
+
+app = Flask(__name__)
+
+
+app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://root:ggininder5487@127.0.0.1:3306/nkust_club"
+
+db = SQLAlchemy(app)
+
+class Student(db.Model):
+    __tablename__ = 'student'
+    std_id = db.Column(db.String(12), unique = True, nullable = False, primary_key=True)
+    department = db.Column(db.String(7), unique = False, nullable = False)
+    name = db.Column(db.String(10), unique = True, nullable = False)
+
+    indentitys = db.relationship('Indentity', backref = 'student')
+    
+
+class Club(db.Model):
+    __tablename__ = 'club'
+    club_id = db.Column(db.String(12), unique = True, nullable = False, primary_key=True)
+    name = db.Column(db.String(10), unique = True, nullable = False)
+    indentity = db.relationship('Indentity', backref = 'club')
+
+class Indentity(db.Model):
+    __tablename__ = 'indentity'
+    pid = db.Column(db.Integer, primary_key = True)
+    std_id = db.Column(db.String(12), db.ForeignKey('student.std_id'))
+    club_id = db.Column(db.String(12), db.ForeignKey('club.club_id'))
+    job = db.Column(db.Integer, nullable = False)
+
